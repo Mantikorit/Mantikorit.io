38fc212d8a0ed8f550c52ba5b64a556e316e7df3
Mantikorit <134800091+Mantikorit@users.noreply.github.com>
2025-07-12 00:35:45 +0300

    Delete scripts directory

--- scripts/AutoHeight.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('.header');
-    const footer = document.querySelector('footer');
-    const main = document.querySelector('main');
-    
-    main.style.paddingTop = `${header.offsetHeight}px`;
-    main.style.paddingBottom = `${footer.offsetHeight}px`;
-});
